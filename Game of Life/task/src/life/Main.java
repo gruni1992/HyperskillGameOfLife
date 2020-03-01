eb9968e915670fdad1046f01ebc263cc77ba5477
Tobias Grunwald <tobiasgrunwald92@gmail.com>
2020-03-01 15:26:54 +0100

    solved stage#1

--- Game of Life/task/src/life/Main.java
+++ Game of Life/task/src/life/Main.java
@@ -1,7 +1,33 @@
 package life;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int seed = scanner.nextInt();
+        boolean[][] universe = createUniverse(size, seed);
+        printUniverse(universe);
+    }
+
+    public static void printUniverse(boolean[][] universe) {
+        for (boolean[] line : universe) {
+            StringBuilder builder = new StringBuilder();
+            for (boolean cell : line) {
+                builder.append(cell ? "O" : " ");
+            }
+            System.out.println(builder.toString());
+        }
+    }
+
+    public static boolean[][] createUniverse(int size, int seed) {
+        Random random = new Random(seed);
+        boolean[][] universe = new boolean[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                universe[i][j] = random.nextBoolean();
+            }
+        }
+        return universe;
     }
 }
