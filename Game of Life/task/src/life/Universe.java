affd649a66c01f5ed7e6cfc6b3ab45a760fe02ed
Tobias Grunwald <tobiasgrunwald92@gmail.com>
2020-03-07 16:08:46 +0100

    solved Stage #3

--- Game of Life/task/src/life/Universe.java
+++ Game of Life/task/src/life/Universe.java
@@ -5,6 +5,7 @@ import java.util.Random;
 
 public class Universe {
     private boolean[][] state;
+    int generation = 1;
 
 
     public Universe(){
@@ -29,6 +30,21 @@ public class Universe {
         this.state = universe;
     }
 
+    public void evolve(){
+        generation++;
+        state = Generation.get(state);
+    }
+
+    public int countAlive(){
+        int alive = 0;
+        for (boolean[] line : state) {
+            for (boolean cell : line) {
+                alive += cell ? 1 : 0;
+            }
+        }
+        return alive;
+    }
+
     public boolean[][] getState() {
         return state;
     }
@@ -38,6 +54,8 @@ public class Universe {
     }
 
     public void printState() {
+        System.out.println("Generation: #" + generation);
+        System.out.println("Alive: " + countAlive());
         for (boolean[] line : state) {
             StringBuilder builder = new StringBuilder();
             for (boolean cell : line) {
