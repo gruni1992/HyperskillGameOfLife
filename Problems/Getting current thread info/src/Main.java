2a691b44697f02d56b0a403536bbb7c2cae2c8c0
Tobias Grunwald <tobiasgrunwald92@gmail.com>
2020-03-07 16:30:52 +0100

    solved problem for "Threads as Objects"

---
+++ Problems/Getting current thread info/src/Main.java
@@ -0,0 +1,13 @@
+//class Main{
+    public static void printCurrentThreadInfo() {
+        // get the thread and print its info
+        Thread thread = Thread.currentThread();
+        System.out.println("name: " + thread.getName());
+        System.out.println("priority: " + thread.getPriority());
+    }
+/*
+    public static void main(String[] args) {
+        printCurrentThreadInfo();
+    }
+}
+*/
